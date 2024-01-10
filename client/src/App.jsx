2edde1044daa4e38611689933600648856c0583a
Theo <theo090992@yahoo.fr>
2024-01-10 13:58:32 +0100

    project backup v1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import Conversation from './contracts/Conversation.json';
+import './App.css';
 
 function App() {
   const [accounts, setAccounts] = useState([])
   const [contract, setContract] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [date, setDate] = useState([]);
+  const [content, setContent] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     initWeb3();
@@ -27,7 +31,8 @@ function App() {
       const provider = new ethers.BrowserProvider(window.ethereum)
       const signer = await provider.getSigner()
       const contract = new ethers.Contract(contractAddress, contractABI, signer)
-      setContract(contract)
+      console.log("Contract Interface:", contract.interface.fragments);
+      setContract(contract);
     } catch (error) {
       console.error('Error initializing web3:', error);
     }
@@ -43,27 +48,55 @@ function App() {
     }
   }
 
+  async function handleSubmit() {
+    try {
+      await contract.postMessage(content)
+      setError('')
+    } catch (error) {
+      setError(error.reason)
+    }
+  }
+
   return (
-    <div>
-      <h1>Conversation Contract Interaction</h1>
+    <div className='cvrst-chat'>
+      <div className='cvrst-title'>
+        <h1>Conversation</h1>
+      </div>
       {contract ? (
-        <>
-          <p>Contract connected: {contract.target}</p>
-          <p>Accounts connected: {accounts[0]}</p>
+        <>{/*
+            <div>
+              <p><b>Vous êtes connecté au contrat:</b> {contract.target}</p>
+              <p><b>Compte connecté:</b> {accounts[0]}</p>
+            </div>
+          */}
           {messages.length > 0 && (
             <div>
               <h2>Last 10 Messages:</h2>
               <ul>
                 {messages.map((message, index) => (
-                  <li key={index}>{`User: ${message.user}, Content: ${message.content}, Timestamp: ${message.timestamp}`}</li>
+                  <li key={index}>
+                    {message.user !== "0x0000000000000000000000000000000000000000" ? (
+                      <>
+                        {`User: ${message.user}`} <br />
+                        {`Content: ${message.content}`} <br />
+                        {`Date d'envoie: ${message.timestamp}`} <br /><br />
+                      </>
+                    ) : (
+                      "Utilisateur Non valide"
+                    )}
+                  </li>
                 ))}
               </ul>
             </div>
           )}
+          <input type="text" placeholder="Your message" value={content} onChange={(e) => setContent(e.target.value)}/>
+          <button onClick={handleSubmit}>Submit</button>
         </>
       ) : (
         <p>Connecting to the contract...</p>
       )}
+
+      {error && <p className="error">{error}</p>}
     </div>
   );
 }
